0b406d781844a65829cce1242241d35e8de81bfc
kiririx <kiririx@outlook.com>
2022-04-11 14:33:40 +0800

    init basic code

--- linked_list/linked_list_kr.go
+++ linked_list/linked_list_kr.go
@@ -1,9 +1,11 @@
 package linked_list
 
 type LinkedList struct {
+	nodes []node
 }
 
-func (l *LinkedList) add() {
+func (l *LinkedList) add(v int) {
+	//n := node{v: v}
 
 }
 
@@ -12,4 +14,7 @@ func (l *LinkedList) del() {
 }
 
 type node struct {
+	nextPtr *node
+	lastPtr *node
+	v       int
 }
